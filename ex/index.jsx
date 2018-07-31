3e4f57d9217edc66024800250e4fd1810052dff1
tvasconcelos90 <tiagobiker1@gmail.com>
2018-07-31 20:40:04 -0300

    aula 68

--- ex/index.jsx
+++ ex/index.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
-import fieldReducer from './fieldReducer'
-
-import Field from './field'
+import counterReducer from './counterReducer'
+import Counter from './counter'
 
 const reducers = combineReducers({
-    field: fieldReducer 
+    counter: counterReducer 
 })
 
 ReactDOM.render (
     <Provider store={createStore(reducers)}>
-        <Field initialValue = 'Teste' />
+        <Counter />
     </Provider>
    , document.getElementById('app') 
 )
